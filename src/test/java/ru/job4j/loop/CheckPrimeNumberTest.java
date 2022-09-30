477d697864ad02f6ba817c39364526ace1675dd2
Oksana <crystal-soul@ya.ru>
2022-09-30 17:53:27 +0300

    5.5. Простое число [#171694 #347108]

--- src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -3,7 +3,6 @@ package ru.job4j.loop;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 public class CheckPrimeNumberTest {
     @Test
     public void when5() {
